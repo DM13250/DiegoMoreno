b5a56d4b8fcfc25be9adfe9daa4cd8e325242d6b
DM13250 <dieguitocule@gmail.com>
2023-05-17 15:59:31 +0200

    Sexto Commit

--- src/script.js
+++ src/script.js
@@ -40,16 +40,17 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("drupal");
+        habilidades[0].classList.add("drupal");
+        habilidades[1].classList.add("angular");
+        habilidades[2].classList.add("C");
         habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("angular");
-        habilidades[5].classList.add("C");
-        habilidades[6].classList.add("comunicacion");
-        habilidades[7].classList.add("trabajo");
-        habilidades[8].classList.add("creatividad");
-        habilidades[9].classList.add("dedicacion");
+        habilidades[4].classList.add("javascript");
+        habilidades[5].classList.add("bootstrap");
+        habilidades[6].classList.add("htmlcss");
+        habilidades[7].classList.add("comunicacion");
+        habilidades[8].classList.add("trabajo");
+        habilidades[9].classList.add("creatividad");
+        habilidades[10].classList.add("dedicacion");
     }
 }
 window.onscroll = function(){
